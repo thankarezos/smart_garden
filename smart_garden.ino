026081a4c489eda80e58b63dfcee428a37f959e1
thankarezos <thankarezos@gmail.com>
2022-07-02 10:05:57 +0300

    connect to wifi

--- smart_garden.ino
+++ smart_garden.ino
@@ -2,10 +2,31 @@ boolean state;
 int period = 1000;
 #define EXE_INTERVAL_1 1000
 unsigned long lastExecutedMillis_1 = 0;
+#include <ESP8266WiFi.h>  
+#include "wifi.h"
+
 void setup() {
+  Serial.begin(115200);         // Start the Serial communication to send messages to the computer
+  delay(10);
+  Serial.println('\n');
+  
+  WiFi.begin(ssid, password);             // Connect to the network
+  Serial.print("Connecting to ");
+  Serial.print(ssid); Serial.println(" ...");
+
+  int i = 0;
+  while (WiFi.status() != WL_CONNECTED) { // Wait for the Wi-Fi to connect
+    delay(1000);
+    Serial.print(++i); Serial.print(' ');
+  }
+
+  Serial.println('\n');
+  Serial.println("Connection established!");  
+  Serial.print("IP address:\t");
+  Serial.println(WiFi.localIP());         // Send the IP address of the ESP8266 to the computer
+
   
   pinMode(A0, INPUT);
-  Serial.begin(115200);
   Serial.println();
   printState();
 }
